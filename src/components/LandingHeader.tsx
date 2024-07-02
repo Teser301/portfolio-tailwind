750527f456c630196505b0d0d8cb85876916d2fe
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2024-07-02 12:23:06 +0300

    typo

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -4,13 +4,13 @@ function LandingHeader() {
   const model = useGLTF("./room.glb");
   return (
     <>
-      <div className="grid grid-cols-2">
-        <div className="w-full flex flex-col pb-5  mt-11">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Front end developer</h1>
-          <p>My name is Carlo Aavekukk. I am a front end developer for 7+ years. This is my portfolio website. Designed to showcase my work and my skills.</p>
+      <div className="flex">
+        <div className="w-full max-w-3xl flex flex-col pl-40 pr-20 py-20 justify-center bg-dSecondary ">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Looking for a Front end developer ?</h1>
+          <p>My name is Carlo Aavekukk. I am a front end developer for 6+ years. This is my portfolio website. Designed to showcase my work and my skills.</p>
         </div>
-        <div className="bg-wBackground w-full h-[800px]">
-          <Canvas flat dpr={[1, 2]} camera={{ fov: 25, position: [0, 0, 8] }} shadows>
+        <div className="w-full h-[800px] bg-wBackground">
+          <Canvas flat dpr={[1, 2]} camera={{ fov: 45, position: [0, 1, 5] }} shadows>
             <color attach="background" args={["#020c1a"]} />
             <ambientLight intensity={2} />
             <PresentationControls snap global zoom={0.8} rotation={[0, -Math.PI / 4, 0]} polar={[0, Math.PI / 4]} azimuth={[-Math.PI / 4, Math.PI / 4]}>
