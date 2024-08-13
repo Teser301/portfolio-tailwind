62c02814e0c57cc2faab0da2923924f07f7633e5
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2024-08-13 17:08:32 +0300

    feat: darkmode

--- src/components/LandingPage/LandingPortfolio.tsx
+++ src/components/LandingPage/LandingPortfolio.tsx
@@ -27,7 +27,7 @@ const projects = [
 function Portfolio() {
   return (
     <div className="p-6">
-      <h1 className="text-center text-2xl font-semibold mb-6 text-customGrayText dark:text-white">Portfolio</h1>
+      <h1 className="text-center text-2xl font-semibold mb-6 text-black dark:text-white">Portfolio</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {projects.map((project, index) => (
           <div key={index} className="relative group w-full h-96 bg-cover bg-center rounded-lg overflow-hidden" style={{ backgroundImage: `url(${project.image})` }}>
@@ -36,10 +36,7 @@ function Portfolio() {
             </div>
             <div className="absolute inset-0 bg-customGray bg-opacity-50 opacity-0 group-hover:opacity-100 flex flex-col items-center justify-center transition-opacity duration-300 p-4">
               <p className="text-white mb-2">{project.description}</p>
-              <a
-                href={project.link}
-                className="inline-block text-center text-customGrayText dark:text-white border border-white py-1 px-3 rounded hover:bg-white hover:text-black transition duration-300"
-              >
+              <a href={project.link} className="inline-block text-center text-black dark:text-white border border-white py-1 px-3 rounded hover:bg-white hover:text-black transition duration-300">
                 Visit Project
               </a>
             </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { FaMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa";
 
 function NavBar() {
   const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "light");
@@ -16,14 +18,18 @@ function NavBar() {
     setTheme(theme === "dark" ? "light" : "dark");
   };
   return (
-    <header className="flex justify-between items-center p-5 w-full dark:bg-background border-b-2 border-b-dPrimary">
+    <header className="flex justify-between items-center p-5 w-full border-b-2 border-black dark:border-white border-b-dPrimary">
       <div>
-        <a href="https://www.linkedin.com/in/carlo-aavekukk-22b92b1b2/">Carlo</a>
+        <a href="https://www.linkedin.com/in/carlo-aavekukk-22b92b1b2/" className="text-black dark:text-white">
+          Carlo
+        </a>
       </div>
       <div>
-        <a href="/home">Content</a>
-        <button onClick={toggleTheme} className="ml-4 p-2 bg-gray-200 dark:bg-gray-800 dark:text-gray-200">
-          {theme === "dark" ? "Light Mode" : "Dark Mode"}
+        <a href="/home" className="text-black dark:text-white">
+          Content
+        </a>
+        <button onClick={toggleTheme} className="ml-4 p-2 text-black dark:text-white">
+          {theme === "dark" ? <FaSun /> : <FaMoon />}
         </button>
       </div>
     </header>
