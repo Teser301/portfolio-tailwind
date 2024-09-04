298f4ac978f499048fbf21953052f22c7ee1160d
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2024-09-04 15:05:25 +0300

    refactor: move office model

--- src/components/LandingPage/OfficeModel.tsx
+++ src/components/LandingPage/OfficeModel.tsx
@@ -1,9 +1,16 @@
 import { useGLTF, PresentationControls } from "@react-three/drei";
 
-function OfficeModel() {
+const OfficeModel: React.FC = () => {
   const model = useGLTF("./room.glb");
   return (
-    <PresentationControls snap global zoom={0.8} rotation={[0, -Math.PI / 4, 0]} polar={[0, Math.PI / 4]} azimuth={[-Math.PI / 4, Math.PI / 4]}>
+    <PresentationControls
+      snap
+      global
+      zoom={0.8}
+      rotation={[0, -Math.PI / 4, 0]}
+      polar={[0, Math.PI / 4]}
+      azimuth={[-Math.PI / 4, Math.PI / 4]}
+    >
       <pointLight position={[0, 1, 0]} color={0xffffff} intensity={10} />
       <spotLight position={[-2, 0, 1]} color={0xffffff} intensity={25} />
       <group position={[0, -1, 0]} dispose={null}>
@@ -11,6 +18,6 @@ function OfficeModel() {
       </group>
     </PresentationControls>
   );
-}
+};
 
 export default OfficeModel;
