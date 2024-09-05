37a1c0d074ac13a5d6ab256317e1e9ed320ecfb4
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2024-09-05 15:41:34 +0300

    refactor: update skills data

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,19 +1,17 @@
 import { Skills } from "../types/skills";
 import ReactLogo from "../assets/icons/react.svg";
 import HTMLLogo from "../assets/icons/html5.svg";
+import CSSLogo from "../assets/icons/css.svg";
 import JavaScriptLogo from "../assets/icons/javascript.svg";
 import PythonLogo from "../assets/icons/python.svg";
 import TailwindLogo from "../assets/icons/tailwind.svg";
 import WordpressLogo from "../assets/icons/wordpress.svg";
+import HandlebarsLogo from "../assets/icons/handlebars.svg";
 
 export const skills: Skills[] = [
     {
         name: "React",
         logo: ReactLogo,
-    },
-    {
-        name: "HTML",
-        logo: HTMLLogo,
     },
     {
         name: "JavaScript",
@@ -30,5 +28,17 @@ export const skills: Skills[] = [
     {
         name: "Tailwind",
         logo: TailwindLogo,
+    },
+    {
+        name: "Handlebars",
+        logo: HandlebarsLogo,
+    },
+    {
+        name: "CSS",
+        logo: CSSLogo,
+    },
+    {
+        name: "HTML",
+        logo: HTMLLogo,
     },
 ];
