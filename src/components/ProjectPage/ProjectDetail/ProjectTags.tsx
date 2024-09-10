af8b91c1ae7ae530d6ec151b5e084e16e0756389
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2024-09-10 14:30:38 +0300

    add: tagged colors

--- src/components/ProjectPage/ProjectDetail/ProjectTags.tsx
+++ src/components/ProjectPage/ProjectDetail/ProjectTags.tsx
@@ -13,6 +13,11 @@ const ProjectTags: React.FC<ProjectTagsProps> = ({ tags }) => {
     javascript:
       "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300",
     php: "bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300",
+    html: "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300",
+    css: "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300",
+    sass: "bg-pink-100 text-pink-800 dark:bg-pink-900 dark:text-pink-300",
+    bootstrap:
+      "bg-indigo-100 text-indigo-800 dark:bg-indigo-900 dark:text-indigo-300",
   };
 
   return (
