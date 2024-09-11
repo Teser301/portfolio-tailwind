02041ce7f9566c352f5898dc8cee6a02a5fb518b
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2024-09-11 12:53:46 +0300

    add: scrollbar style

--- src/components/ProjectPage/ProjectDetail.tsx
+++ src/components/ProjectPage/ProjectDetail.tsx
@@ -10,50 +10,38 @@ interface ProjectDetailProps {
 
 const ProjectDetail: React.FC<ProjectDetailProps> = ({ selectedProject }) => {
   return (
-    <div
-      className={`hidden md:flex flex-1 bg-backgroundPrimary dark:bg-gray-800 p-6 shadow-md rounded-lg flex-col justify-start h-screen transition-opacity duration-500 overflow-y-auto`}
-    >
+    <div className="hidden md:flex flex-1 flex-col justify-start sticky h-[800px]  p-6 top-20 bg-backgroundPrimary shadow-md rounded-lg">
       {selectedProject ? (
-        <>
+        <div className="flex flex-col overflow-x-hidden overflow-y-scroll custom-scrollbar-container">
           <img
             src={selectedProject.image}
             alt={selectedProject.title}
-            className="w-full h-96 object-cover object-top rounded-md"
+            className="w-full h-80 object-cover object-top rounded-md"
           />
           <ProjectStatus
             status={selectedProject.status}
             link={selectedProject.link}
           />
-          <h2 className="text-2xl font-bold mt-4 text-gray-800 dark:text-gray-200">
-            {selectedProject.title}
-          </h2>
+          <h2 className="text-2xl font-bold mt-4">{selectedProject.title}</h2>
           <ProjectTags tags={selectedProject.type} />
           <ProjectFeatures features={selectedProject.features} />
           <div>
-            <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
-              Description:
-            </h3>
-            <p className="mt-2 text-gray-600 dark:text-gray-400">
-              {selectedProject.description}
-            </p>
+            <h3 className="text-lg font-semibold ">Description:</h3>
+            <p className="mt-2">{selectedProject.description}</p>
           </div>
 
           {selectedProject.bigFeatureTitle &&
             selectedProject.bigFeatureDescription && (
               <div className="mt-2">
-                <h3 className="text-lg  font-semibold text-gray-800 dark:text-gray-200">
+                <h3 className="text-lg  font-semibold">
                   {selectedProject.bigFeatureTitle}
                 </h3>
-                <p className="mt-2 text-gray-600 dark:text-gray-400">
-                  {selectedProject.bigFeatureDescription}
-                </p>
+                <p className="mt-2">{selectedProject.bigFeatureDescription}</p>
               </div>
             )}
-        </>
+        </div>
       ) : (
-        <p className="text-gray-500 dark:text-text text-center">
-          Hover over a project to see its details.
-        </p>
+        <p className="text-center">Hover over a project to see its details.</p>
       )}
     </div>
   );
