808fd4adc61dcae5b052475c055e8266477ae243
Carlo Aavekukk <30408387+Teser301@users.noreply.github.com>
2024-09-15 13:14:58 +0300

    refactor: ts > tsx

--- src/hooks/useFilter.tsx
+++ src/hooks/useFilter.tsx
@@ -1,12 +1,11 @@
-// useFilter.ts
 import { useContext } from "react";
 import { FilterContext, FilterContextProps } from "../context/FilterContext"; // Adjust the path if necessary
 
 // Custom hook for using the context
 export const useFilter = (): FilterContextProps => {
-    const context = useContext(FilterContext);
-    if (!context) {
-        throw new Error("useFilter must be used within a FilterProvider");
-    }
-    return context;
+  const context = useContext(FilterContext);
+  if (!context) {
+    throw new Error("useFilter must be used within a FilterProvider");
+  }
+  return context;
 };
